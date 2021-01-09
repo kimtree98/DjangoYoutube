3e14ba7e4e1af1a1cbfbc57193a00fa228777bb7
Kimtree <nawookim@naver.com>
2021-01-09 12:57:54 +0900

    additional files and changes for HerokuYoutubeStore

--- MyYoutubeStore/settings.py
+++ MyYoutubeStore/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
